71363f7837e45d8adcb72eb8ad403b4175628670
krynju <gulinski.krystian@gmail.com>
2021-07-25 08:53:34 +0200

    add storage based on union, file input constructor

--- test/table.jl
+++ test/table.jl
@@ -32,12 +32,24 @@ using CSV
         size = 15_000
         io = IOBuffer()
         CSV.write(io, [(a=10, b=20) for i in 1:size])
-        d = CSV.read(take!(io), DTable) # TODO: figure out how to add a keyword arg here (for now default value in constructor)
+        d = CSV.read(take!(io), DTable)
         CSV.write(io, [(a=10, b=20) for i in 1:size])
         # or
         d2 = DTable(CSV.File(take!(io)); chunksize=1_000)
         @test fetch(d) == DataFrame([(a=10, b=20) for i in 1:size])
         @test fetch(d2) == DataFrame([(a=10, b=20) for i in 1:size])
+    end
+
+    @testset "constructors - file input" begin
+        n = 20
+        size = 1000
+        ios = [IOBuffer() for _ in 1:n]
+        data = [(a=rand(size), b=rand(size)) for _ in 1:n]
+        arr = [Arrow.write(ios[idx], data[idx]) for idx in 1:n]
+
+        da = DTable([string(i) for i in 1:n], (x)-> Arrow.Table(take!(ios[tryparse(Int64,x)])))
+        db = vcat([DataFrame(d) for d in data]...)
+        @test fetch(da) == db
     end
 
     @testset "map" begin
@@ -53,8 +65,8 @@ using CSV
         sample_df = () -> DataFrame(a=rand(size), b=rand(size))
         s = sample_df()
         d = DTable(s; chunksize=100)
-        s_r = map(x-> x.a + x.b, eachrow(filter(x-> x.a > 0.5, s)))
-        d_r = fetch(map(x-> x.a + x.b, filter(x-> x.a > 0.5, d)))
+        s_r = filter(x-> x.a > 0.5, s)
+        d_r = fetch(filter(x-> x.a > 0.5, d))
         @test s_r == d_r
     end
 end
